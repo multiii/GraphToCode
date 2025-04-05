95b1b7c6edbcf10ae0b868c5ba5c221a2af35b61
Ankit M <ankitmalhotra788@gmail.com>
2025-04-05 14:20:06 -0700

    starting llm compiler

---
+++ my-app/server_side.js
@@ -0,0 +1,14 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({apiKey: process.env.GOOGLE_KEY});
+
+export async function prompt(prompt) {
+    const reponse = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: prompt,
+    });
+    console.log(reponse.text);
+}
+
+await prompt("hello there!");
+
