932ae0bda0675bb300bcc304f463256f15279714
Ankit M <ankitmalhotra788@gmail.com>
2025-04-05 17:53:43 -0700

    remove extra and fix package json

--- my-app/thealgorithm.js
+++
@@ -1,7 +0,0 @@
-import Parser from 'tree-sitter'
-import CPP from 'tree-sitter-cpp'
-import fs from 'fs'
-
-export function parseFile () {
-    const isCpp = filePath.endsWith('.cpp') || filePath.endsWith('.hpp')
-}
